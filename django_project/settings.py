ff85469ba386a9a8a0def163f96467d35434a691
shinhosuck <shinhosuck1973@gmail.com>
2021-01-24 04:12:01 +0800

    update

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'y*)0i$mlw+^&$)lt7obt6x_1!i6!)7x=&ct7sb-k#tfu3rl4$_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.43.107", "192.168.43.248", "127.0.0.1", "mydjangomusic.herokuapp.com"]
+ALLOWED_HOSTS = ["mydjangomusic.herokuapp.com"]
+# ALLOWED_HOSTS = ["192.168.43.107", "192.168.43.248", "127.0.0.1"]
 
 # Application definition
 
