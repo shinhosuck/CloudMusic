46c8a7b585a73e6133850db212ff3348046b5bf1
shinhosuck <shinhosuck1973@gmail.com>
2021-07-12 19:24:26 +0800

    update

--- venv/bin/django-admin.py
+++ venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/Users/anderson/Documents/GitHub/Django-Mac/Django/venv/bin/python3
+#!/home/anderson/Documents/CloudMusic/venv/bin/python3
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
